5dea85905f984d167c4c852bfc7723cbeaefb270
Mohamed Alaa <100510627+mohalaa7220@users.noreply.github.com>
2024-03-21 23:56:44 +0200

    .

--- courses/models.py
+++ courses/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
 
 # ============ course model ================
 class Course(models.Model):
-    coursename = models.CharField(max_length=100)
+    coursename = models.CharField(
+        max_length=100, verbose_name=_("Course Name"))
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     update_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
@@ -14,11 +16,13 @@ class Course(models.Model):
 
 # ============ section model ================
 class Section(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    sectiontitle = models.CharField(max_length=1000)
-    description = models.TextField()
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, verbose_name=_("Course"))
+    sectiontitle = models.CharField(
+        max_length=1000, verbose_name=_("Section Title"))
+    description = models.TextField(verbose_name=_("Description"))
     pictureSection = models.ImageField(
-        upload_to='pictures/', null=True, blank=True, verbose_name="image")
+        upload_to='pictures/', null=True, blank=True, verbose_name=_("Image Section"))
     section_Slug = models.SlugField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     update_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
@@ -34,9 +38,11 @@ class Section(models.Model):
 
 # ============ level model ================
 class Level(models.Model):
-    levelTitle = models.CharField(max_length=100, null=True)
-    section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    levelDescription = models.TextField()
+    levelTitle = models.CharField(
+        max_length=100, null=True, verbose_name=_("Level Title"))
+    section = models.ForeignKey(
+        Section, on_delete=models.CASCADE, verbose_name=_("Section"))
+    levelDescription = models.TextField(verbose_name=_("Level Description"))
    # video = models.FileField(null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     update_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
@@ -48,12 +54,12 @@ class Level(models.Model):
 # ============ news model ================
 class News(models.Model):
 
-    news_title = models.CharField(max_length=100)
-    news_desc = models.TextField()
+    news_title = models.CharField(max_length=100, verbose_name=_("News Title"))
+    news_desc = models.TextField(verbose_name=_("News Description"))
     news_date = models.DateField(null=True, blank=True)
-    lessonsNum = models.IntegerField()
+    lessonsNum = models.IntegerField(verbose_name=_("Lesson Number"))
     newsImage = models.ImageField(
-        upload_to='newsImage/', null=True, blank=True, verbose_name="image")
+        upload_to='newsImage/', null=True, blank=True, verbose_name=_("News Image"))
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     update_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 

f9b1eb53fb01cdb7f5864b6b9b750cd25ebeeeaf
Mohamed Alaa <100510627+mohalaa7220@users.noreply.github.com>
2024-03-21 16:30:32 +0200

    .

--- courses/admin.py
+++ courses/admin.py
@@ -10,8 +10,26 @@ class CourseAdmin(admin.ModelAdmin):
     list_filter = ('created_at', 'update_at')
 
 
-admin.site.register(Section)
+# =============== section Model =================
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    search_fields = ('sectiontitle', 'description',)
+    list_filter = ('created_at', 'update_at',)
+    list_display = ('sectiontitle', 'course', 'created_at', 'update_at',)
 
-admin.site.register(Level)
 
-admin.site.register(News)
+# =============== Level Model =================
+@admin.register(Level)
+class LevelAdmin(admin.ModelAdmin):
+    search_fields = ('levelTitle', 'levelDescription',)
+    list_filter = ('created_at', 'update_at',)
+    list_display = ('levelTitle', 'section', 'created_at', 'update_at',)
+
+
+# =============== News Model =================
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    search_fields = ('news_title', 'news_desc',)
+    list_filter = ('created_at', 'update_at',)
+    list_display = ('news_title', 'news_date', 'lessonsNum',
+                    'created_at', 'update_at',)

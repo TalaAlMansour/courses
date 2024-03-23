5857cfefc8bd1f4526a9a786e3ba1abae035ccf9
TalaAlMansour <127053366+TalaAlMansour@users.noreply.github.com>
2024-03-23 11:10:58 +0300

    adding-t

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -56,6 +57,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'courses.courses_list.courses_list'
             ],
         },
     },

1ffb84d83063549a1e5a1acfbdb943dcf72d828f
TalaAlMansour <127053366+TalaAlMansour@users.noreply.github.com>
2024-03-23 20:51:11 +0300

    add

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'courses',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
